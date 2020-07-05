571b5414e871d4697b9437022dbeb83d116b2d54
Dominator <amitsamant@outlook.com>
2020-07-06 01:23:37 +0530

    Implemented differs list styles

---
+++ Shared/ListStyleView.swift
@@ -0,0 +1,95 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by dominator on 05/07/20.
+//
+
+import SwiftUI
+
+struct ListStyleView: View {
+    
+    @State var selectedIndex = 3
+    
+    var list: some View{
+        List{
+            ForEach(0..<10) { row in
+                Text("Row \(row)")
+            }
+        }
+    }
+    
+    var styledList: some View{
+        Group{
+            switch selectedIndex{
+            #if os(iOS)
+            // InsetGroupedListStyle is only available in iOS
+            case 0:
+                list
+                    .listStyle(InsetGroupedListStyle())
+            #endif
+            case 1:
+                list
+                    .listStyle(InsetListStyle())
+            case 2:
+                list
+                    .listStyle(SidebarListStyle())
+            case 3:
+                list
+            default:
+                EmptyView()
+            }
+        }
+    }
+    
+    var stylePicker: some View{
+        Picker(selection: $selectedIndex, label: Text("List Style"), content: {
+            #if os(iOS)
+            Text("Inset group").tag(0)
+            #endif
+            Text("Inset").tag(1)
+            Text("Sidebar style").tag(2)
+            Text("Default").tag(3)
+        })
+        .labelsHidden()
+    }
+    
+    var macOS: some View{
+        NavigationView{
+            styledList
+                .navigationTitle("List Styles")
+            Text("Select segment to change list style")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+        }
+        //adding toolbar inside the navigationview produces unwanted crashes try out your self if you want.
+        .toolbar {
+            stylePicker
+        }
+    }
+    
+    
+    var iOS: some View{
+        NavigationView{
+            styledList
+                .navigationTitle("List Styles")
+                .toolbar {
+                    stylePicker
+                        .pickerStyle(SegmentedPickerStyle())
+                }
+        }
+    }
+    
+    var body: some View{
+        #if os(iOS)
+        return iOS
+        #elseif os(macOS)
+        return macOS
+        #endif
+    }
+}
+
+struct ListStyleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListStyleView()
+    }
+}
--- Shared/ListStylesApp.swift
+++ Shared/ListStylesApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ListStylesApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ListStyleView()
         }
     }
 }
