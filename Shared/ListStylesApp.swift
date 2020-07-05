09f7e731a29a5d5057147968e76cd30d22502b7a
Dominator <amitsamant@outlook.com>
2020-07-05 18:42:50 +0530

    Initial Commit

---
+++ Shared/ListStylesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListStylesApp.swift
+//  Shared
+//
+//  Created by dominator on 05/07/20.
+//
+
+import SwiftUI
+
+@main
+struct ListStylesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
